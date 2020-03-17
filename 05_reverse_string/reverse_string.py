538300e7395a9164da2e5e6a71278ab482624fc5
Terry Chiem <terrychiem@gmail.com>
2020-03-16 17:13:54 -0700

    initial commit 1-10

---
+++ 05_reverse_string/reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_string(phrase):
+    """Reverse string,
+
+        >>> reverse_string('awesome')
+        'emosewa'
+
+        >>> reverse_string('sauce')
+        'ecuas'
+    """
+    reverse = reversed(list(phrase))
+    return ''.join(reverse)
---
+++ 09_is_palindrome/is_palindrome.py
@@ -0,0 +1,29 @@
+def is_palindrome(phrase):
+    """Is phrase a palindrome?
+
+    Return True/False if phrase is a palindrome (same read backwards and
+    forwards).
+
+        >>> is_palindrome('tacocat')
+        True
+
+        >>> is_palindrome('noon')
+        True
+
+        >>> is_palindrome('robert')
+        False
+
+    Should ignore capitalization/spaces when deciding:
+
+        >>> is_palindrome('taco cat')
+        True
+
+        >>> is_palindrome('Noon')
+        True
+    """
+    phrase = phrase.replace(' ', '').lower()
+
+    for i in range(len(phrase)):
+        if phrase[i] != phrase[-1 - i]:
+            return False
+    return True
